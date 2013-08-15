2b7922d81fe9783b3c998796f2ebd5b419d084a8
lkonatham <lkonatham@gmail.com>
2013-08-15 15:22:27 -0500

    Update fbldr_content_creator.js

--- applications/forms-app/javascripts/fbldr_content_creator.js
+++ applications/forms-app/javascripts/fbldr_content_creator.js
@@ -388,7 +388,7 @@ jive.fbldr.ContentCreator = function(template, form) {
             else {
                 var document = response.data;
                 document.contentType = "document";
-                document.href = "/docs/DOC-" + response.data.id+"/edit?ID="+response.data.id;
+                document.href = "/docs/DOC-" + response.data.id+"/edit";
                 document.subject = data.subject;
 
                 if (template.content.includeAttachment) {
